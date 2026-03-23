3f113136148b8fd61e5b1bb4b6075680ef294bf6
Анастасия <nastkalol807@gmail.com>
2026-03-23 15:10:47 +0300

    создание проекта

---
+++ Praktika/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Reka
+{
+    class River
+    {
+        public string name;
+        public double basinArea;
+        public double length;
+        public River()
+        {
+            name = "Not specified";
+            basinArea = 0;
+            length = 0;
+        }
+        public River(string name, double basinArea, double length)
+        {
+            this.name = name;
+            this.basinArea = basinArea;
+            this.length = length;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Basin area: {basinArea} km2");
+            Console.WriteLine($"Length: {length} km");
+            Console.WriteLine();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            River r1 = new River();
+            r1.name = "Nile";
+            r1.basinArea = 3400000;
+            r1.length = 6650;
+            r1.Print();
+            River r2 = new River("Amazon", 7050000, 6400);
+            r2.Print();
+            Console.ReadLine();
+        }
+    }
+}
